21500d2b5511581ac5ab5d0cb6214d83e6a1fc77
Sam070602 <samyakjain02@gmail.com>
2021-04-10 22:59:30 +0530

    added files

---
+++ Python/numpy_training_file_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.array([1, 2, 3])
+print(a)
+
+b = np.array([[9, 0, 8, 0, 7, 0], [6, 0, 5, 0, 4, 0]])
+print(b)
+
+print(a.ndim) #Get DImension
+print(b.ndim)
+
+print(a.shape)
+print(b.shape)
